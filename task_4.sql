95c152ba8fb08fb1aadd9867003a84a356fbcc2b
NiGedeon <nigideon300@gmail.com>
2024-10-17 03:54:01 -0700

    task_4.sql file is added!

---
+++ task_4.sql
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import error
+
+def database_name(dbname):
+	try:
+		connection = mysql.connector.connect(
+			host = "localhost",
+			username = "root",
+			password = "yourpassword",
+			database = dbname
+		)
+
+		if connection.isconnected():
+			cursor = connection.cursor()
+			cursor.execute("DESC Books;")
+			cursor.close()
+			connection.close()
+
+	except error as e:
+		print(f"Error:{e}")
+
+database_name("alx_book_store")
