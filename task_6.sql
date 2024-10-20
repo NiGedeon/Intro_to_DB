930b52d17429413cbc962668fffb9ee7945a01f3
NiGedeon <nigideon300@gmail.com>
2024-10-20 02:10:45 -0700

    a task_6.sql file is added

--- task_6.sql
+++ task_6.sql
@@ -6,26 +6,31 @@ connection = mysql.connector.connect(
     database = "alx_book_store"
 ) 
 cursor = connection.cursor() 
-sql = "INSERT INTO customers (customer_id,customer_name,email,address) VALUES(%s,%s,%s,%s),(%s,%s,%s,%s),(%s,%s,%s,%s)" 
+sql = "INSERT INTO customers (customer_id,customer_name,email,address) VALUES(%s,%s,%s,%s)" 
 val1 =(
     2,
     "Blessing Malik",
     "bmalik@sandtech.com",
     "124 Happiness Ave."
 )
+cursor.execute(sql,val1)
+cursor.commit()
 val2 =(
     3,
     "Obed Ehoneah",
     "eobed@sandtech.com",
     "125 Happiness Ave."
 )
+cursor.execute(sql,val2)
+cursor.commit()
+
 val3 =(
     4,
     "Nehemial Kamolu",
     "nkamolu@sandtech.com",
     "126 Happiness Ave."
 )
-cursor.execute(sql, val1,val2,val3) 
+cursor.execute(sql,val3) 
 cursor.commit() 
 print(mycursor.rowcount, "record(s) inserted.") 
 cursor.close() 
